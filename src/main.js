e5b27bb4716d9557f150d01fa12f01b518f480f9
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-03-30 16:54:09 +0200

    remove icon default

--- src/main.js
+++ src/main.js
@@ -9,12 +9,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-regular-svg-icons'
 import { faStar as faStarFull } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret,faStar,faStarFull)
+library.add(faStar,faStarFull)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
